9352e2138972467a240582f00ac148110ade2e13
sheena <sheena@puppet.com>
2021-06-09 10:31:56 +0100

    (maint) incorporate review comments

--- lib/puppet/provider/check_http/check_http.rb
+++ lib/puppet/provider/check_http/check_http.rb
@@ -27,29 +27,36 @@ class Puppet::Provider::CheckHttp::CheckHttp
 
     # This callback provides the exception that was raised in the current try, the try_number, the elapsed_time for all tries so far, and the time in seconds of the next_interval.
     do_this_on_each_retry = Proc.new do |exception, try, elapsed_time, next_interval|
-      log "#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next try."
+      context.info("#{exception.class}: '#{exception.message}' - #{try} tries in #{elapsed_time} seconds and #{next_interval} seconds until the next try.")
     end
 
     context.processing(uri.to_s, {}, {}, message: 'checking http') do
-      Retriable.retriable(on: Timeout::Error, tries: should_hash[:retries], max_elapsed_time: should_hash[:request_timeout], base_interval: base_interval, max_interval: should_hash[:max_backoff], on_retry: do_this_on_each_retry) do
+      base_interval = 0
+      retried = 0
+      Retriable.retriable(tries: should_hash[:retries], max_elapsed_time: should_hash[:request_timeout], base_interval: base_interval, max_interval: should_hash[:max_backoff], multiplier: should_hash[:exponential_backoff_base], on_retry: do_this_on_each_retry) do
+        retried +=1
+
+        # Update the check_http provider to wait for backoff ** (exponential_backoff_base * (retries - 1) seconds between attempts (up to max_backoff)
+        base_interval = should_hash[:backoff] ** should_hash[:exponential_backoff_base] * (retried - 1)
+        context.info("The base_interval is '#{base_interval}' and retrying for '#{retried}' time")
+
         response = Net::HTTP.get_response(uri)
 
         # Success being defined as having one of the expected_statuses and the body of the response matches body_matcher
         if (should_hash[:expected_statuses].include? response.code.to_i)
-          context.info("The return response #{response.code} is matching with the expected_statuses #{should_hash[:expected_statuses]}")
+          context.info("The return response '#{response.code}' is matching with the expected_statuses '#{should_hash[:expected_statuses]}'")
           if (response.body.match(should_hash[:body_matcher]))
-            context.info("The return response body '#{response.body[..99]}` is matching with body_matcher '#{should_hash[:body_matcher]}'")
+            context.info("The return response body '#{response.body[0..99]}' is matching with body_matcher '#{should_hash[:body_matcher]}.to_s'")
           else
-            raise Puppet::Error, "check_http response body check failed. The return response body #{response.body} is not matching body_matcher #{should_hash[:body_matcher]}"
-            return false
+            raise Puppet::Error, "check_http response body check failed. The return response body '#{response.body[0..99]}' is not matching body_matcher '#{should_hash[:body_matcher]}.to_s'"
           end
-          context.info("successfully connected to #{name}")
+          context.info("Successfully connected to '#{name}'")
           return true
         else
-          raise Puppet::Error, "check_http response code check failed. The return response #{response.code} is not matching with the expected_statuses #{should_hash[:expected_statuses]}"
-          return false
+          raise Puppet::Error, "check_http response code check failed. The return response '#{response.code}' is not matching with the expected_statuses '#{should_hash[:expected_statuses]}.to_s'"
         end
       end
+      return false
     end
   end
 end
--- spec/unit/puppet/provider/check_http/check_http_spec.rb
+++ spec/unit/puppet/provider/check_http/check_http_spec.rb
@@ -1,52 +1,45 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
+require 'pry'
 
 ensure_module_defined('Puppet::Provider::CheckHttp')
 require 'puppet/provider/check_http/check_http'
 
 RSpec.describe Puppet::Provider::CheckHttp::CheckHttp do
   subject(:provider) { described_class.new }
+  subject(:retriable) { described_class.new }
 
   let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
 
-  describe '#get' do
+  let(:should_uri) { 'abc.test.net' }
+  let(:uri) { instance_double('URI', 'uri') }
+  let(:should_hash) { { name: 'foo', url: should_uri, ensure: 'present',expected_statuses: [200], body_matcher: /Google/, request_timeout: 30, retries: 3, backoff: 1, exponential_backoff_base:2, max_backoff:40, timeout:60 } }
+  let(:is_hash) { { name: 'foos', url: should_uri, ensure: 'present',expected_statuses: [200], body_matcher: /Google/, request_timeout: 30, retries: 3, backoff: 1, exponential_backoff_base:2, max_backoff:40, timeout:60 } }
+  let(:base_interval) {}
+  let(:proc) { instance_double('Proc', 'proc') }
+  let(:do_this_on_each_retry) { instance_double('Proc', 'do_this_on_each_retry') }
+  let(:exception) {}
+  let(:try) {}
+  let(:elapsed_time) {}
+  let(:next_interval) {}
+  let(:response) {}
+  let(:retriable) { instance_double('Retriable', 'retriable') }
+  let(:net_http) { instance_double('Net::HTTP', 'net_http') }
+
+  describe 'get(context)' do
     it 'processes resources' do
-      expect(context).to receive(:debug).with('Returning pre-canned example data')
-      expect(provider.get(context)).to eq [
-        {
-          name: 'foo',
-          ensure: 'present',
-        },
-        {
-          name: 'bar',
-          ensure: 'present',
-        },
-      ]
+      expect(provider.get(context)).to eq []
     end
   end
 
-  describe 'create(context, name, should)' do
-    it 'creates the resource' do
-      expect(context).to receive(:notice).with(%r{\ACreating 'a'})
-
-      provider.create(context, 'a', name: 'a', ensure: 'present')
-    end
-  end
-
-  describe 'update(context, name, should)' do
-    it 'updates the resource' do
-      expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
-
-      provider.update(context, 'foo', name: 'foo', ensure: 'present')
-    end
-  end
-
-  describe 'delete(context, name)' do
-    it 'deletes the resource' do
-      expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
-
-      provider.delete(context, 'foo')
+  describe 'insync?(context, name, attribute_name, is_hash, should_hash) without Retry' do
+    it 'processes resources' do
+      allow(context).to receive(:debug)
+      allow(URI).to receive(should_uri).and_return uri
+      expect(context).to receive(:debug).with('Checking whether foo is up-to-date')
+      expect(context).to receive(:processing).with("abc.test.net", {}, {}, {:message=>"checking http"})
+      expect(provider.insync?(context, 'foo', 'foo', is_hash, should_hash)).to be(nil)
     end
   end
 end
