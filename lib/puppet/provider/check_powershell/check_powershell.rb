cc675a923bd5aaee585a0c65c3954e6aba635b2d
sheena <sheena@puppet.com>
2021-07-08 09:15:14 +0100

    (maint)

--- lib/puppet/provider/check_powershell/check_powershell.rb
+++ lib/puppet/provider/check_powershell/check_powershell.rb
@@ -14,9 +14,29 @@ class Puppet::Provider::CheckPowershell::CheckPowershell
 
   def set(context, changes); end
 
+  def upgrade_message
+    # Message used when an invalid PS version is found.
+    CHECK_POWERSHELL_UPGRADE_MSG = <<-UPGRADE
+    The check_powershell requires PowerShell version %{required} - current version %{current}
+    To enable this resource, please install the latest version of WMF 5+ from Microsoft.
+    UPGRADE
+
+    required_version = Gem::Version.new('5.0.10586.117')
+    installed_version = Gem::Version.new(Facter.value(:powershell_version))
+
+    if installed_version >= required_version
+      context.info("Satisfying powershell_version")
+    else
+      Puppet.warn_once(*params)
+    end
+  end
+
   def ps_manager
-    debug_output = Puppet::Util::Log.level == :debug
-    Pwsh::Manager.instance(Pwsh::Manager.powershell_path, Pwsh::Manager.powershell_args)
+    unless Pwsh::Manager.windows_powershell_supported?
+      upgrade_message
+      debug_output = Puppet::Util::Log.level == :debug
+      Pwsh::Manager.instance(Pwsh::Manager.powershell_path, Pwsh::Manager.powershell_args)
+    end
   end
 
   # Update the check_powershell provider to use the above attributes to execute up to retries number of times
@@ -34,29 +54,35 @@ class Puppet::Provider::CheckPowershell::CheckPowershell
     Retriable.retriable(tries: should_hash[:retries], max_elapsed_time: should_hash[:request_timeout], max_interval: should_hash[:max_backoff],
 multiplier: should_hash[:exponential_backoff_base], on_retry: do_this_on_each_retry) do
 
-      result     = ps_manager.execute(should_hash[:command])
-      stdout     = output[:stdout]
-      native_out = output[:native_stdout]
-      stderr     = output[:stderr]
-      exit_code  = output[:exitcode]
+      begin
+        result     = ps_manager.execute(should_hash[:command])
+        stdout     = result[:stdout]
+        native_out = result[:native_stdout]
+        stderr     = result[:stderr]
+        exit_code  = result[:exitcode]
 
-      unless stderr.nil?
-        stderr.each { |e| Puppet.debug "STDERR: #{e.chop}" unless e.empty? }
+        unless stderr.nil?
+          stderr.each { |e| Puppet.debug "STDERR: #{e.chop}" unless e.empty? }
+        end
+
+        Puppet.debug "STDERR: #{result[:errormessage]}" unless result[:errormessage].nil?
+
+        output = Puppet::Util::Execution::ProcessOutput.new(stdout.to_s + native_out.to_s, exit_code)
+
+        #binding.pry
+      rescue Exception => exception
+        #binding.pry
+        puts exception.message
+        puts exception.backtrace.inspect
       end
-
-      Puppet.debug "STDERR: #{output[:errormessage]}" unless output[:errormessage].nil?
-
-      output = Puppet::Util::Execution::ProcessOutput.new(stdout.to_s + native_out.to_s, exit_code)
-
-      binding.pry
-      unless should_hash[:expected_exitcode].include? output[:exitcode].to_i
-        raise Puppet::Error, "check_powershell exitcode check failed. The return exitcode '#{output[:exitcode]}' is not matching with the expected_exitcode '#{should_hash[:expected_exitcode]}'"
+      unless should_hash[:expected_exitcode].include? output[exit_code].to_i
+        raise Puppet::Error, "check_powershell exitcode check failed. The return exitcode '#{output[exit_code]}' is not matching with the expected_exitcode '#{should_hash[:expected_exitcode]}'"
       end
-      context.debug("The return exitcode '#{output[:exitcode]}' is matching with the expected_exitcode '#{should_hash[:expected_exitcode]}'")
-      unless output[:stdout].match(should_hash[:output_matcher])
-        raise Puppet::Error, "check_powershell output check failed. The return output '#{output[:stdout]}' is not matching output_matcher '#{should_hash[:output_matcher]}'"
+      context.debug("The return exitcode '#{output[exit_code]}' is matching with the expected_exitcode '#{should_hash[:expected_exitcode]}'")
+      unless output[stdout].match(should_hash[:output_matcher])
+        raise Puppet::Error, "check_powershell output check failed. The return output '#{output[stdout]}' is not matching output_matcher '#{should_hash[:output_matcher]}'"
       end
-      context.debug("The return output '#{output[:stdout]}' is matching with output_matcher '#{should_hash[:output_matcher]}'")
+      context.debug("The return output '#{output[stdout]}' is matching with output_matcher '#{should_hash[:output_matcher]}'")
       context.debug("Successfully executed the command '#{should_hash[:command]}'")
       return true
     end
